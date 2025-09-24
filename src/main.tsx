0fccba0709eac5e2a74e013a0bf40a300f8cb88c
Adrian Campana <adrianphcampana12@gmail.com>
2025-09-24 11:11:03 -0300

    Implement mobile fixes to eliminate overflow issues and enhance viewport handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./mobile-fixes.css";
 import App from "./App.tsx";
 
 import { PrimeReactProvider } from "primereact/api";
